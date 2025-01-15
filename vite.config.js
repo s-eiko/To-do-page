f0ad6b787b1aa02d332c355e4890a1640d10c301
Mariana <marianaseikofuku@gmail.com>
2025-01-15 20:03:20 -0300

    fix: corrigindo problemas de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import { vitePlugin as remix } from "@remix-run/dev"
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    remix({
-      basename: "/",
-      buildDirectory: "build",
-    })],
+  plugins: [react()],
 })
